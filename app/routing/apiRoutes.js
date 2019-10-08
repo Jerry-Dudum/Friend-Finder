2e5c09ac68669d7e0259215b74fb47799cf83ffa
Jerry Dudum <jsdudum@gmail.com>
2019-10-07 20:47:55 -0700

    finsihed the app and everything seems to be working fine now

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,13 +2,40 @@ var friends = require("../data/friend.js");
 
 module.exports = function (app) {
     //GET method to retrieve entire friends list
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
-    })
+    });
 
     //POST method to push newly made friend into the array
-    app.post("/api/friends", function(req, res) {
-        friends.push(req.body);
-        res.json(true);
-    })
-}
+    app.post("/api/friends", function (req, res) {
+
+        var user = req.body;
+
+        for (var i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i])
+        }
+
+        var bestFriendIndex = 0;
+        var lowestDiff = 40;
+
+        for (var i = 0; i < friends.length; i++) {
+
+            var scoreDiff = 0;
+
+            for (var j = 0; j < 10; j++) {
+                var diff = Math.abs(user.scores[j] - friends[i].scores[j]);
+                scoreDiff += diff;
+            }
+
+
+            if (scoreDiff < lowestDiff) {
+                bestFriendIndex = i;
+                lowestDiff = scoreDiff
+            }
+        }
+
+        friends.push(user);
+
+        res.json(friends[bestFriendIndex]);
+    });
+};
