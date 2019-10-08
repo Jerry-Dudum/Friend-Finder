4e0276142be647e74f6e9ffee219a053bccf92f5
Jerry Dudum <jsdudum@gmail.com>
2019-10-07 17:58:14 -0700

    added apiRoutes and htmlRoutes requirement inside server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var app = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
