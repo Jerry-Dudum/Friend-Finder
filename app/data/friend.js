3ee35c924de6007eed27a2241624a0a393a29603
Jerry Dudum <jsdudum@gmail.com>
2019-10-07 18:07:13 -0700

    added friends to the friends array will change scores once I make the survey questions

---
+++ app/data/friend.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Iron Man",
+        photo: "https://cdn.pinkvilla.com/files/styles/contentpreview/public/iron-man-phase-4_0.jpg?itok=zzbN51r7",
+        score: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Hulk",
+        photo: "https://i0.wp.com/www.oswegonian.com/wp-content/uploads/2019/04/BestClips.png?resize=715%2C400&ssl=1",
+        score: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Thor",
+        photo: "https://s3.r29static.com/bin/entry/ce2/720x864,85/2169674/image.webp",
+        score: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Captain America",
+        photo: "https://i.pinimg.com/originals/d5/81/79/d58179ac3e5781037d0fbf2bf3a97cdc.jpg",
+        score: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Dr. Strange",
+        photo: "https://pbs.twimg.com/profile_images/817369685192605696/iWkgUdBf.jpg",
+        score: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Black Panther",
+        photo: "https://www.counterpunch.org/wp-content/dropzone/2018/02/black-panther.jpg",
+        score: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+];
+
+module.exports = friends;
