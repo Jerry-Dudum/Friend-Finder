268e367aaec622a9fd8075d557cb1b1b2f99829d
Jerry Dudum <jsdudum@gmail.com>
2019-10-07 19:16:19 -0700

    added path package as well as the apiRoutes get and post methods needed

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+var friends = require("../data/friend.js");
+
+module.exports = function (app) {
+    //GET method to retrieve entire friends list
+    app.get("/api/frinds", function(req, res) {
+        res.json(friends);
+    })
+
+    //POST method to push newly made friend into the array
+    app.post("/api/friends", function(req, res) {
+        friends.push(req.body);
+        res.json(true);
+    })
+}
